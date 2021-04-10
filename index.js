a08d4bc90c3094e65430174b533167aae1859b4a
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-09 21:48:58 -0500

    feat(index.js, index.html): Put the user input into a form and called that form in Javascript

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
-// Goal: 
+// Goal: Call the form in HTML 
 //  We are going to sort this file by variables, functions and execution
 
 // VARIABLES
-const endpoint = 'https://www.boredapi.com/api';
 const activityInput = document.querySelector('.input');
 const searchButton = document.querySelector('.searchButton');
 
-let answers = [];
-
-// FUNCTIONS
-
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log('submitted!');
-    const { bored } = fetchActivity();
-    console.log(bored);
-    const searchBoxInput = e.currentTarget.searchBox.value
-    console.log(searchBoxInput);
-    const participantInput = e.currentTarget.participants.value
-    console.log(participantInput);
-    const typeInput = e.currentTarget.type.value
-    console.log(typeInput);
-
-}
-function handleError(err) {
-    console.log('Whats that?');
-    console.log(err);
-}
-
-async function fetchActivity() {
-    const response = await fetch(endpoint);
-    const activity = response.json();
-    console.log(activity);
-}
-
-// EXECUTION
-
-const activityPromise = fetch(endpoint);
-activityPromise.then(response => {
-        return response.json();
-}).then(data => {
-        console.log(data);
-}).catch(handleError)
-
 activityInput.addEventListener('submit', handleSubmit);
-
-fetchActivity();
-
-
-// function mirrorToLocalStorage() {
-//     console.log('Saving to Local Storage');
-//     localStorage.setActivity('input', JSON.stringify(items))
-// }
-
-// function addSearch(event) {
-//     const searchActivity = event.currentTarget
-// }
 
 
