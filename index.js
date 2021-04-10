f4a6a20c3d017d55730be20855239543f57eaba0
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-09 22:03:54 -0500

    feat(index.js): Created a handleSubmit event that would submit the input data

--- index.js
+++ index.js
@@ -5,6 +5,27 @@
 const activityInput = document.querySelector('.input');
 const searchButton = document.querySelector('.searchButton');
 
+// FUNCTIONS
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted!');
+    const { bored } = fetchActivity();
+    console.log(bored);
+    const searchBoxInput = e.currentTarget.searchBox.value
+    console.log(searchBoxInput);
+    const participantInput = e.currentTarget.participants.value
+    console.log(participantInput);
+    const typeInput = e.currentTarget.type.value
+    console.log(typeInput);
+
+}
+function handleError(err) {
+    console.log('Whats that?');
+    console.log(err);
+}
+
+// Execution
 activityInput.addEventListener('submit', handleSubmit);
 
 
