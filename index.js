6418528aaff95028588d25371a96474b43a822fd
fab514 <68705672+fab514@users.noreply.github.com>
2021-04-13 00:32:29 -0400

    Feat/22 button event (#29)
    * feat(index.html): For the participants input a min of 1 and a max of 100 was added into the html
    
    * fix(activity.html): Link to activity page was switched to home page
    
    * fix(quiz.html): link to quiz.html was fixed to index.html
    
    * feat(index.js): search and lucky button were selected from html
    
    * fix(index.html): Put the inputs into a form tag, changed the names to better organize the js
    
    * feat(index.js): added functions for clicking a button, fetch from the api and showing data in the console
    
    * doc(jsNotes.md): Adjusted notes adding project goals.
    
    * fiix(README.md): documented relative changes to the html)
    
    * feat(index.js, index.html): Put the user input into a form and called that form in Javascript
    
    * feat(index.js): Created a handleSubmit event that would submit the input data
    
    * fix(index.js): removed data that did not belong to the present tickets

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-// Goal: Call the form in HTML 
+// Goal: Create a button event that when clicked the information will be shown in the console. 
 //  We are going to sort this file by variables, functions and execution
 
 // VARIABLES
 const activityInput = document.querySelector('.input');
+const searchButton = document.querySelector('.searchButton');
 
+// FUNCTIONS
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted!');
+    const searchBoxInput = e.currentTarget.searchBox.value
+    console.log(searchBoxInput);
+    const participantInput = e.currentTarget.participants.value
+    console.log(participantInput);
+    const typeInput = e.currentTarget.type.value
+    console.log(typeInput);
+
+}
+
+// Execution
+activityInput.addEventListener('submit', handleSubmit);
 
 
