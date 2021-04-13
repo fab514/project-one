4ead30041839718aa6f429757489dc2063513e9f
fab514 <68705672+fab514@users.noreply.github.com>
2021-04-12 23:46:32 -0400

    Feat/21 form js (#28)
    * feat(index.html): For the participants input a min of 1 and a max of 100 was added into the html
    
    * fix(activity.html): Link to activity page was switched to home page
    
    * fix(quiz.html): link to quiz.html was fixed to index.html
    
    * feat(index.js): search and lucky button were selected from html
    
    * fix(index.html): Put the inputs into a form tag, changed the names to better organize the js
    
    * feat(index.js): added functions for clicking a button, fetch from the api and showing data in the console
    
    * doc(jsNotes.md): Adjusted notes adding project goals.
    
    * fiix(README.md): documented relative changes to the html)
    
    * feat(index.js, index.html): Put the user input into a form and called that form in Javascript
    
    * fix(index.js): Removed data not apart of ticket

---
+++ index.js
@@ -0,0 +1,8 @@
+// Goal: Call the form in HTML 
+//  We are going to sort this file by variables, functions and execution
+
+// VARIABLES
+const activityInput = document.querySelector('.input');
+
+
+
