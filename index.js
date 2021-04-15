c98ea73d8938eb9db9d448a269ee980c2fb383a6
fab514 <68705672+fab514@users.noreply.github.com>
2021-04-15 01:39:17 -0400

    Feat/35 fetch with button (#36)
    * feat(index.html): For the participants input a min of 1 and a max of 100 was added into the html
    
    * fix(activity.html): Link to activity page was switched to home page
    
    * fix(quiz.html): link to quiz.html was fixed to index.html
    
    * feat(index.js): search and lucky button were selected from html
    
    * fix(index.html): Put the inputs into a form tag, changed the names to better organize the js
    
    * feat(index.js): added functions for clicking a button, fetch from the api and showing data in the console
    
    * doc(jsNotes.md): Adjusted notes adding project goals.
    
    * fiix(README.md): documented relative changes to the html)
    
    * feat(index.js, index.html): Put the user input into a form and called that form in Javascript
    
    * feat(index.js): Created a handleSubmit event that would submit the input data
    
    * fix(index.js): removed data that did not belong to the present tickets
    
    * feat(index.js): Added an endpoint for the bored api and created a fetchActivity function to connect to the api
    
    * feat(index.js): called the fetchActivity function
    
    * fix: In fetch activity function fixed the res.json to res.data
    
    * feat: attached the fetch function to the handleSubmit
    
    * fix: Only have fetchActivity in the bored variable
    
    * fix: moved the bored variable underneith the input variables so it would be run after
    
    * fix: add error handling to fetch request and modified activity variable with response.json

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// Goal: Create a button event that when clicked the information will be shown in the console. 
+
+// Goal: Attach the fetchActivity function to the handleClick so bored api will be activated on button click
 //  We are going to sort this file by variables, functions and execution
 
 // VARIABLES
+const endpoint = 'https://www.boredapi.com/api';
 const activityInput = document.querySelector('.input');
 const searchButton = document.querySelector('.searchButton');
 
 // FUNCTIONS
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
     e.preventDefault();
     console.log('submitted!');
     const searchBoxInput = e.currentTarget.searchBox.value
@@ -16,10 +18,22 @@ function handleSubmit(e) {
     console.log(participantInput);
     const typeInput = e.currentTarget.type.value
     console.log(typeInput);
-
+    const bored = await fetchActivity();
+    console.log('bored', bored);
 }
 
+
+async function fetchActivity() {
+    let response;
+    let activity;
+    try {
+      response = await fetch(endpoint); 
+      activity = await response.json(); 
+      console.log('activity', activity);
+    } catch(error) {
+        console.error('error', error);
+    }
+    return activity;
+}
 // Execution
 activityInput.addEventListener('submit', handleSubmit);
-
-
