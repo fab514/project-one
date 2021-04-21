871c8cea5669fc351d2996a3e245f716461eac0e
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-20 22:46:47 -0500

    feat: api request now accepts values

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //  We are going to sort this file by variables, functions and execution
 
 // VARIABLES
-const endpoint = 'https://www.boredapi.com/api';
+const baseEndpoint = 'https://www.boredapi.com/api/activity';
 const activityInput = document.querySelector('.input');
 const searchButton = document.querySelector('.searchButton');
 
@@ -12,28 +12,60 @@ const searchButton = document.querySelector('.searchButton');
 async function handleSubmit(e) {
     e.preventDefault();
     console.log('submitted!');
-    const searchBoxInput = e.currentTarget.searchBox.value
-    console.log(searchBoxInput);
     const participantInput = e.currentTarget.participants.value
     console.log(participantInput);
     const typeInput = e.currentTarget.type.value
     console.log(typeInput);
-    const bored = await fetchActivity();
+    
+    const participantQuery = participantInput !== '' ? `participants=${participantInput}` : '';
+    console.log('participantQuery:', participantQuery);
+    const typeQuery = typeInput !== '' ? `type=${typeInput}` : '';
+    console.log('typeQuery:', typeQuery);
+
+    let constructedQuery = '';
+    switch (true) {
+        case Boolean(participantQuery && !typeQuery):
+            constructedQuery = `?${participantQuery}`
+            break;
+        case Boolean(participantQuery && typeQuery):
+            constructedQuery = `?${participantQuery}&${typeQuery}`;
+            break;
+        case Boolean(!participantQuery && typeQuery):
+            constructedQuery = `?${typeQuery}`;
+            break
+        case Boolean(!participantQuery && !typeQuery):
+            console.log('No queries were filled out');
+            constructedQuery = '';
+            break
+        default: 
+            console.log('None of the cases in the switch statement matched. Running with a empty query')
+            constructedQuery='';
+            break
+    }
+    const queryEndpoint = `${baseEndpoint}${constructedQuery}`;
+    
+    const bored = await fetchActivity(queryEndpoint);
     console.log('bored', bored);
 }
 
 
-async function fetchActivity() {
+async function fetchActivity(fetchQueryEndpoint) {
     let response;
     let activity;
     try {
-      response = await fetch(endpoint); 
+      response = await fetch(fetchQueryEndpoint, {
+        headers: {
+            Accept: 'application/json',
+        }
+    }); 
       activity = await response.json(); 
       console.log('activity', activity);
     } catch(error) {
         console.error('error', error);
     }
     return activity;
+    
 }
 // Execution
 activityInput.addEventListener('submit', handleSubmit);
+
