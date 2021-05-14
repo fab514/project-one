7b6ee13c104eae70eccc49add98ea98f82eb9be0
fab514 <68705672+fab514@users.noreply.github.com>
2021-05-14 10:49:20 -0500

    Feat/24 local storage (#40)
    * feat: api request now accepts values
    
    * fix: white space
    
    * fix: created a more efficient version of the fetch of activity
    
    * feat: activities are stored to localStorage
    
    * feat: error should appear if there is no activity available

--- index.js
+++ index.js
@@ -7,50 +7,74 @@ const activityInput = document.querySelector('.input');
 const searchButton = document.querySelector('.searchButton');
 
 // FUNCTIONS
-function inputQuery (inputArray) {
-    return inputArray
-        .filter((input) => input !== "")
-        .map((input, index) => (index === 0 ? `?${input}` : `&${input}`)) 
-        .join("");
+
+
+function inputQuery(inputArray) {
+  return inputArray
+    .filter((input) => input !== "")
+    .map((input, index) => (index === 0 ? `?${input}` : `&${input}`))
+    .join("");
+}
+
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 async function handleSubmit(e) {
-    e.preventDefault();
-    console.log('submitted!');
-    const participantInput = e.currentTarget.participants.value
-    console.log(participantInput);
-    const typeInput = e.currentTarget.type.value
-    console.log(typeInput);
-    
-    const participantQuery = participantInput !== '' ? `participants=${participantInput}` : '';
-    console.log('participantQuery:', participantQuery);
-    const typeQuery = typeInput !== '' ? `type=${typeInput}` : '';
-    console.log('typeQuery:', typeQuery);
+  e.preventDefault();
+  const participantInput = e.currentTarget.participants.value
+  const typeInput = e.currentTarget.type.value
 
-    const constructedQuery = inputQuery([participantQuery, typeQuery]);
-    
-    const queryEndpoint = `${baseEndpoint}${constructedQuery}`;
-    
-    const bored = await fetchActivity(queryEndpoint);
-    console.log('bored', bored);
+  const participantQuery = participantInput !== '' ? `participants=${participantInput}` : '';
+  const typeQuery = typeInput !== '' ? `type=${typeInput}` : '';
+
+  const constructedQuery = inputQuery([participantQuery, typeQuery]);
+
+  const queryEndpoint = `${baseEndpoint}${constructedQuery}`;
+
+  const bored = await fetchActivity(queryEndpoint);
+
+  if (bored && bored.error) {
+
+    const myError = document.querySelector('.wrapper');
+
+    removeChildNodes(myError);
+
+    const myHTML = document.createElement('h2');
+    myHTML.textContent = 'Oops! No Activity Found!';
+    myError.appendChild(myHTML);
+
+    const img = document.createElement('img');
+    img.src = "https://cdn.pixabay.com/photo/2017/01/09/12/49/mistake-1966448_1280.jpg"
+    img.width = 500;
+    img.height = 250;
+    img.alt = 'question kid!';
+    myError.appendChild(img);
+
+  } else {
+    localStorage.setItem('bored', JSON.stringify(bored));
+    window.location = './activity.html';
+  }
+
+
+
 }
-
 async function fetchActivity(fetchQueryEndpoint) {
-    let response;
-    let activity;
-    try {
-      response = await fetch(fetchQueryEndpoint, {
-        headers: {
-            Accept: 'application/json',
-        }
-    }); 
-      activity = await response.json(); 
-      console.log('activity', activity);
-    } catch(error) {
-      console.error('error', error);
-    }
-    return activity;
-    
+  let response;
+  let activity;
+  try {
+    response = await fetch(fetchQueryEndpoint, {
+      headers: {
+        Accept: 'application/json',
+      }
+    });
+    activity = await response.json();
+  } catch (error) {
+    console.error('error', error);
+  }
+  return activity;
 }
 // Execution
 activityInput.addEventListener('submit', handleSubmit);
