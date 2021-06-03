7a0a9b9450cd62326f31711c13317993ad77f91d
felicia <you@example.com>
2021-06-02 22:12:11 -0500

    feat(quiz.js): Created a new data structure to filter out results

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,102 +3,855 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 const containerElement = document.querySelector('.container')
 const apiKey = 'fM2ZjlMUQUm5t6tgHe3IyQYj9Q5NHDPs';
 
-const atHomeRelaxedResults = [
-  'Read a New Book',
-  'Work on a Puzzle',
-  'Take a nap',
-  'Binge Watch Series',
-  'Find New Music',
-  'Sit on the Outside Deck',
-  'Home Spa Night',
-  'Create a Photo Album',
-  'Play a Video or Computer Game',
-  'Watch a Movie',
-  'Try Out New Cooking Recipes',
-  'Listen to a Podcast',
-  'Order Delivery',
-  'Video Game Tournament',
-  'Board Game Night',
-];
+let resultArray = [
+  {
+    name: 'Read a New Book',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Work on a Puzzle',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Take a nap',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Binge Watch Series',
+    categories: [
+      'Solo',
+      'Duo',
+      'BFF', 
+      'Date',
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Find New Music',
+    categories: [
+      'Solo',
+      'Duo',
+      'BFF', 
+      'Date',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Sit on the Outside Deck',
+    categories: [
+      'Solo',
+      'Duo',
+      'BFF', 
+      'Group',
+      'Date',
+      'Outdoor',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Home Spa Night',
+    categories: [
+      'Solo',
+      'Duo',
+      'BFF', 
+      'Date',
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Create a Photo Album',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Play a Video or Computer Game',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Watch a Movie',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF',
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Try Out New Cooking Recipes',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Listen to a Podcast',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Order Delivery',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF',
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Video Game Tournament',
+    categories: [
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Board Game Night',
+    categories: [
+      'Group',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Home Alone? Set traps to get rid of burglers',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Home',
+      'Active'
+    ]
+  },
+  {
+    name: 'Room Makeover',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Active'
+    ]
+  },
+  {
+    name: 'Spring Cleaning',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date', 
+      'Indoor', 
+      'Home',
+      'Active'
+    ]
+  },
+  {
+    name: 'Try New Drink Recipes',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date',
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Relaxing'
+    ]
+  },
+  {
+    name: 'Learn a new skill',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor', 
+      'Outdoor',
+      'Home',
+      'Active',
+      'Recreation', 
+    ]
+  },
+  {
+    name: 'Plant a Home Garden',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Home workout or yoga',
+    categories: [
+      'Solo',
+      'Duo', 
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Active',
+    ]
+  },
+  {
+    name: 'Cooking/Baking Competition',
+    categories: [
+      'Duo', 
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Home',
+      'Active',
+    ]
+  },
+  {
+    name: 'Throw a Party',
+    categories: [
+      'Group',
+      'Indoor',
+      'Home',
+      'Active',
+    ]
+  },
+  {
+    name: 'Throw a Potluck Dinner',
+    categories: [
+      'Group',
+      'Indoor',
+      'Home',
+      'Active',
+    ]
+  },
+  {
+    name: 'Try Out a New Restaurant',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'Find an Antique or Thrift Shop',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'Find a local Coffee Shop or Bakery',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'I\'ll take you to the Candy Shop',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'Take up a Photography Hobby',
+    categories: [
+      'Solo',
+      'Indoor', 
+      'Public',
+      'Outdoor', 
+      'Nature'
+    ]
+  },
+  {
+    name: 'Search a Record Store',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'Perform at an Open Mic night or Karaoke',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor', 
+      'Public',
+    ]
+  },
+  {
+    name: 'Play Games at an Arcade',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'See a Movie at the Theatre',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Go see a Comedian',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Go on a Dinner cruize',
+    categories: [
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Dancing Lessons',
+    categories: [
+      'Duo',
+      'Date',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Find a way out of an Escape Room',
+    categories: [
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Become a Detective at a Murder Mystery Party',
+    categories: [
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Join a Trivia Night',
+    categories: [
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Community Volunteer',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Shopping Scavenger Hunt',
+    categories: [
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Play Billiards or go Bowling',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Visit an Aquarium or Museum',
+    categories: [
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Go to a Live Concert',
+    categories: [
+      'Duo',
+      'Date', 
+      'BFF',
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Go to the Gym',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Find a Board Game Cafe',
+    categories: [
+      'Group',
+      'Indoor',
+      'Public',
+    ]
+  },
+  {
+    name: 'Watch the Sunrise/ or Sunset',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Go on a Picnic',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date', 
+      'BFF',
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Take a Hike',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Go Camping',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Relaxing by a Bonfire',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Walk around a Botanical Garden or Arboretum',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Train for 5k',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Gone Fishing',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Ride a Bike',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Explore a New City',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Indoor',
+      'Public',
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Take your dog to the Park',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Fly a Kite',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Go to a Zoo',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Go Berry or Apple Picking(anything in season)',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Horseback Riding',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Nature',
+    ]
+  },
+  {
+    name: 'Take a Day Trip out of town or state',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Find a GoKart Track',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Watch Live Sports',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Go shopping at a Street or Art Fair',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Geocaching around your town',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Go to the Beach',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'If winter go Skiing or sledding',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'If summer, rent a Boat or Jetski',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Go Kayaking, canoeing or paddle boating at a lake or river',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Find a Frisbee Golf Course',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Have fun at a Water Park or Pool',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Find Drive-in Movie(Yes there are still some out there)',
+    categories: [
+      'Solo',
+      'Duo',
+      'Date',
+      'BFF', 
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Play Tag or Hide and Seek',
+    categories: [
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
+  {
+    name: 'Find a group of friends to play Kickball, Basketball or Softball',
+    categories: [
+      'Group', 
+      'Outdoor',
+      'Recreation',
+    ]
+  },
 
-const atHomeActiveResults = [
-  'Room Makeover',
-  'Spring Cleaning',
-  'Try New Drink Recipes',
-  'Walk around Neighborhood',
-  'Plant a Home Garden',
-  'Home workout or yoga',
-  'Learn a new skill',
-  'Cooking/Baking Competition',
-  'Throw a Party',
-  'Throw a Potluck Dinner',
-  'Set traps to get rid of burglers',
 ]
-
-const indoorPublicResults = [
-  'Try Out a New Restaurant',
-  'Find an Antique or Thrift Shop',
-  'Find a local Coffee Shop or Bakery',
-  'I\'ll take you to the Candy Shop',
-  'Take up a Photography Hobby',
-  'Search a Record Store',
-  'Perform at an Open Mic night or Karaoke',
-  'Play Games at an Arcade',
-  'See a Movie at the Theatre',
-  'Go see a Comedian',
-  'Go on a Dinner cruize',
-  'Join a Trivia Night',
-  'Community Volunteer',
-  'Grocery Shopping',
-  'Shopping Scavenger Hunt',
-  'Join a Dance Class',
-  'Play Pool or go Bowling',
-  'Visit an Aquarium or Museum',
-  'Go to a Live Concert',
-  'Throw a Photo Shoot',
-  'Go to the Gym'
-]
-
-const natureResults = [
-  'Watch the Sunrise/ or Sunset',
-  'Go on a Picnic',
-  'Take a Hike',
-  'Go Camping',
-  'Relaxing by a Bonfire',
-  'Walk around a Botanical Garden or Arboretum',
-  'Train for 5k',
-  'Gone Fishing',
-  'Ride a Bike',
-  'Explore a New City',
-  'Take up a Photography Hobby',
-  'Take your dog to the Park',
-  'Fly a Kite',
-  'Have a Photo Shoot',
-  'Go to a Zoo',
-  'Go Berry or Apple Picking(anything in season)',
-  'Horseback Riding',
-]
-
-const outdoorActivityResults = [
-  'Take a Day Trip out of town or state',
-  'Find a GoKart Track',
-  'Watch Live Sports',
-  'Go shopping at a Street or Art Fair',
-  'Geocaching around your town',
-  'Explore a New City',
-  'Take up a Photography Hobby',
-  'Go to the Beach',
-  'If winter go Skiing or sledding',
-  'If summer, rent a Boat or Jetski',
-  'Have a photo shoot',
-  'Go Kayaking, canoeing or paddle boating at a lake or river',
-  'Find a Frisbee Golf Course',
-  'Have fun at a Water Park or Pool',
-  'Find Drive-in Movie(Yes there are still some out there)',
-  'Play Tag or Hide and Seek',
-  'Find a group of friends to play Kickball, Basketball or Softball',
-]
-
 
 function startQuiz() {
   showTextNode(1)
@@ -121,6 +874,7 @@ function showTextNode(textNodeIndex) {
     }
   })
 }
+
 
 function randomItemFromArray(arr, not) {
   const item = arr[Math.floor(Math.random() * arr.length)];
@@ -151,10 +905,12 @@ function renderResult(resultsId) {
 
 function selectAnswer(answerObject) {
   const nextTextNodeId = answerObject.nextText
+  resultArray = resultArray.filter(result => result.categories.includes(answerObject.filter))
+  console.log('resultArray', resultArray)
   if (typeof answerObject.nextText === 'number') {
     showTextNode(nextTextNodeId)
   } else {
-    renderResult(nextTextNodeId)
+    renderResult(resultArray.map(remainingItems => remainingItems.name))
   }
 }
 
@@ -190,14 +946,17 @@ const textNodes = [
       {
         answer: 'One',
         nextText: 3,
+        filter: 'Solo'
       },
       {
         answer: 'Two',
         nextText: 2,
+        filter: 'Duo'
       },
       {
         answer: 'Three or More',
-        nextText: 3
+        nextText: 3,
+        filter: 'Group'
       },
     ]
   },
@@ -209,10 +968,12 @@ const textNodes = [
       {
         answer: 'Going on a date and loving it!',
         nextText: 3,
+        filter: 'Date'
       },
       {
         answer: 'Spending time with my awesome bestie!',
         nextText: 3,
+        filter: 'BFF'
       },
     ]
   },
@@ -224,10 +985,12 @@ const textNodes = [
       {
         answer: 'Indoor',
         nextText: 4,
+        filter: 'Indoor'
       },
       {
         answer: 'Outdoor',
-        nextText: 5
+        nextText: 5,
+        filter: 'Outdoor'
       },
     ]
   },
@@ -239,25 +1002,29 @@ const textNodes = [
       {
         answer: 'I want to stay at home!',
         nextText: 6,
+        filter: 'Home'
       },
       {
         answer: 'Spend the night on the Town!',
-        nextText: indoorPublicResults
+        nextText: 'result',
+        filter: 'Public'
       },
     ]
   },
 
   {
     id: 5,
-    question: 'Out in Nature or an Outdoor Activity?',
+    question: 'Out in Nature or a Recreational Activity?',
     answers: [
       {
         answer: 'Spending time with nature!',
-        nextText: natureResults
+        nextText: 'result',
+        filter: 'Nature'
       },
       {
-        answer: 'Active outdoor activity please!',
-        nextText: outdoorActivityResults
+        answer: 'I\'m having Parks and Rec Vibes',
+        nextText: 'result',
+        filter: 'Recreation'
       },
     ]
   },
@@ -268,14 +1035,18 @@ const textNodes = [
     answers: [
       {
         answer: 'I like being Active Indoors!',
-        nextText: atHomeActiveResults
+        nextText: 'result', 
+        filter: 'Active'
       },
       {
         answer: 'I need a Relaxing Day!',
-        nextText: atHomeRelaxedResults,
+        nextText: 'result',
+        filter: 'Relaxing'
       },
     ]
   }
 ]
+
+console.log('textNodes', textNodes)
 
 startQuiz();
